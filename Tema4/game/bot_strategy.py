99f6979ad93b87549df2af6c8f043f15d337eb6f
Matei <matei_alex1998@yahoo.ro>
2019-10-29 14:56:55 -0700

    Finished Alpha-Beta Pruning

--- Tema4/game/bot_strategy.py
+++ Tema4/game/bot_strategy.py
@@ -46,5 +46,42 @@ class BotStrategy:
         return selected_state, max_layer_score
 
     @staticmethod
-    def alpha_beta(state):
-        pass
+    def alpha_beta_maximum(state, alpha, beta, look_ahead):
+        if look_ahead <= 0:
+            return state, state.score()
+        max_value = -(12 * 14)
+        selected_state = None
+        max_layer = BotStrategy.states_after_bot_moved(state)
+        for max_layer_state in max_layer:
+            returned_state, returned_score = BotStrategy.alpha_beta_minimum(max_layer_state, alpha, beta, look_ahead)
+            if max_value < returned_score:
+                max_value = returned_score
+                selected_state = max_layer_state
+            alpha = max(max_value, alpha)
+            if alpha >= beta:
+                break
+        return selected_state, max_value
+
+    @staticmethod
+    def alpha_beta_minimum(state, alpha, beta, look_ahead):
+        if look_ahead <= 0:
+            return state.score()
+        min_value = 12 * 14
+        selected_state = None
+        min_layer = BotStrategy.states_after_human_moved(state)
+        for min_layer_state in min_layer:
+            returned_state, returned_score = BotStrategy.alpha_beta_maximum(min_layer_state, alpha, beta,
+                                                                            look_ahead - 1)
+            if min_value > returned_score:
+                min_value = returned_score
+                selected_state = min_layer_state
+            beta = min(min_value, beta)
+            if beta <= alpha:
+                break
+        return selected_state, min_value
+
+    @staticmethod
+    def alpha_beta(state, look_ahead=2):
+        alpha = -(12 * 14)
+        beta = 12 * 14
+        return BotStrategy.alpha_beta_maximum(state, alpha, beta, look_ahead)
